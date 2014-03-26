fe374745bfd71742bc777a27cf6fa5ac3e45267e
Gabriel Naiman <gnaiman@keepcon.com>
2014-03-26 15:49:22 -0300

    Change key separator. Added sections to key.

--- lib/asynchronic/data_store/key.rb
+++ lib/asynchronic/data_store/key.rb
@@ -2,12 +2,18 @@ module Asynchronic
   module DataStore
     class Key < String
     
+      SEPARATOR = '|'
+      
       def initialize(key)
         super key.to_s
       end
 
       def [](key)
-        self.class.new "#{self}:#{key}"
+        self.class.new [self,key].join(SEPARATOR)
+      end
+
+      def sections
+        split SEPARATOR
       end
 
     end
--- spec/data_store/key_spec.rb
+++ spec/data_store/key_spec.rb
@@ -2,35 +2,42 @@ require 'minitest_helper'
 
 describe Asynchronic::DataStore::Key do
 
+  Key = Asynchronic::DataStore::Key
+
   it 'Return the namespace' do
-    key = Asynchronic::DataStore::Key.new('foo')
+    key = Key.new 'foo'
     key.must_equal 'foo'
   end
 
   it 'Prepend the namespace' do
-    key = Asynchronic::DataStore::Key.new('foo')
-    key['bar'].must_equal 'foo:bar'
+    key = Key.new 'foo'
+    key['bar'].must_equal 'foo|bar'
   end
 
   it 'Work in more than one level' do
-    key_1 = Asynchronic::DataStore::Key.new('foo')
-    key_2 = Asynchronic::DataStore::Key.new(key_1['bar'])
-    key_2['baz'].must_equal 'foo:bar:baz'
+    key_1 = Key.new 'foo'
+    key_2 = Key.new key_1['bar']
+    key_2['baz'].must_equal 'foo|bar|baz'
   end
 
   it 'Be chainable' do
-    key = Asynchronic::DataStore::Key.new('foo')
-    key['bar']['baz'].must_equal 'foo:bar:baz'
+    key = Key.new 'foo'
+    key['bar']['baz'].must_equal 'foo|bar|baz'
   end
 
   it 'Accept symbols' do
-    key = Asynchronic::DataStore::Key.new(:foo)
-    key[:bar].must_equal 'foo:bar'
+    key = Key.new :foo
+    key[:bar].must_equal 'foo|bar'
   end
 
   it 'Accept numbers' do
-    key = Asynchronic::DataStore::Key.new('foo')
-    key[3].must_equal 'foo:3'
+    key = Key.new 'foo'
+    key[3].must_equal 'foo|3'
+  end
+
+  it 'Split sections' do
+    key = Key.new(:foo)[:bar][:buz]
+    key.sections.must_equal %w(foo bar buz)
   end
   
 end
